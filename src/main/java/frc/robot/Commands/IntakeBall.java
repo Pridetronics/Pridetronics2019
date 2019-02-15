09b3d077f6356bb2087000207ecedf5b9e3acef2
AryanKhanna3853 <aryan.khanna@pridetronics.com>
2019-02-15 15:38:20 -0700

    Wrist and Intake Stop Code

--- src/main/java/frc/robot/Commands/IntakeBall.java
+++ src/main/java/frc/robot/Commands/IntakeBall.java
@@ -37,11 +37,13 @@ public class IntakeBall extends Command {
   // Called once after isFinished returns true
   @Override
   protected void end() {
+    Robot.intake.stop();
   }
 
   // Called when another command which requires one or more of the same
   // subsystems is scheduled to run
   @Override
   protected void interrupted() {
+    end();
   }
 }
--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -76,8 +76,8 @@ public class OI {
     button6.whileHeld(new WristDown());
     // Hatch Panel in/out
     button5 = new JoystickButton(joystick, 5);
-    button5.whenPressed(new PanelPush());
-    button5.whenReleased(new PanelPull());
+    button5.whenPressed(new PushPanel());
+    button5.whenReleased(new PullPanel());
     // Top Hatch on Rocket
 
   }
